c74a2293906db52ca3f802b1e8c39282e9787852
andreas jansson <andreas.s.t.jansson@gmail.com>
2013-11-26 01:15:07 -0500

    optimise

---
+++ optimise_instrumentalness.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cPickle
+import cma
+
+import esn
+import test_data
+
+if __name__ == '__main__':
+
+    input, output, network = test_data.bach()
+
+    optimiser = esn.GeneticOptimiser(network, input, output, 0)
+    params = np.array(optimiser.initial_params())
+    res = cma.fmin(optimiser.evaluate, params, 0.1, maxiter=1)
+
+    filename = '/parent/best_esn_%s.pkl' % res[1]
+    with open(filename, 'w') as f:
+        cPickle.dump(network.serialize(), f)
