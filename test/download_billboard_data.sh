a825ba15e21780c9c589c970c6a1fe6b9e567ab4
andreas jansson <andreas.s.t.jansson@gmail.com>
2013-11-29 19:53:24 -0500

    fix script

--- test/download_billboard_data.sh
+++ test/download_billboard_data.sh
@@ -4,4 +4,4 @@ wget http://ddmal.music.mcgill.ca/datasets/billboard/billboard-2.0.1-mirex.tar.g
 tar xzvf billboard-2.0.1-mirex.tar.gz
 
 wget http://ddmal.music.mcgill.ca/datasets/billboard/billboard-2.0-echonest.tar.gz
-tar xzvf echonest.tar.gz
+tar xzvf billboard-2.0-echonest.tar.gz
